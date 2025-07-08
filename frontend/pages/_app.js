bba403e1af04791b898ad06e68d9336099ec1dc2
k-kimtaI <tezkim414@gmail.com>
2025-07-08 14:44:42 +0300

    Add global Head with title and meta in _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,17 @@
-import "@/styles/globals.css";
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>𝕏-Alpha Feed</title>
+        <meta name="description" content="Crypto and stock analysis from Twitter" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
+
