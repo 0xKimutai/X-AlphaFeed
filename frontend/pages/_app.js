2f71de6390145169671dde8715ce1990528d47f3
k-kimtaI <tezkim414@gmail.com>
2025-07-08 14:51:06 +0300

    Reset: Remove global Head and restore default app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,5 @@
-import Head from 'next/head';
+import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>𝕏-Alpha Feed</title>
-        <meta name="description" content="Crypto and stock analysis from Twitter" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
-
