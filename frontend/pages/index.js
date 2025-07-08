ed9b22850a159ce0fbe10e8ecd453103c78e320d
k-kimtaI <tezkim414@gmail.com>
2025-07-08 13:18:36 +0300

    Fix: Correct backend API URLs

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [tweets, setTweets] = useState([]);
 
   useEffect(() => {
-    axios.get('http://https://x-alphafeed.onrender.com/')
+    axios.get('http://x-alphafeed.onrender.com/')
       .then(res => setTweets(res.data.data))
       .catch(err => console.error('Failed to fetch feed:', err));
   }, []);
